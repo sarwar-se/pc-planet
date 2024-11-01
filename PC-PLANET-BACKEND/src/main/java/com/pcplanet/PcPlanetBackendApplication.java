c0d09ef767f1f71a907070505223bbf90c504439
sarwar-se <sarwar.csesust@gmail.com>
2024-11-01 12:37:40 +0600

    initializes backend

---
+++ PC-PLANET-BACKEND/src/main/java/com/pcplanet/PcPlanetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pcplanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcPlanetBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcPlanetBackendApplication.class, args);
+	}
+
+}
---
+++ PC-PLANET-BACKEND/src/test/java/com/pcplanet/PcPlanetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcplanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcPlanetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
