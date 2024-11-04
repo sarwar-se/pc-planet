3295fe18b5af807e26e717d127f8d587a8b2c55c
sarwar-se <sarwar.csesust@gmail.com>
2024-11-05 01:14:04 +0600

    Add logback & configuring liquibase.

---
+++ PC-PLANET-BACKEND/src/main/java/com/pcplanet/repository/BrandRepository.java
@@ -0,0 +1,7 @@
+package com.pcplanet.repository;
+
+import com.pcplanet.entity.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BrandRepository extends JpaRepository<Brand, Integer> {
+}
